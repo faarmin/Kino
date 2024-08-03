a98383904c8a8815feb584b34c802f69e3811770
faarmin <ferelba8@gmail.com>
2024-08-03 13:30:02 +0200

    Reorganized proyect

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0; URL=src/index.php"/>
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+//Get url variables
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];	
+} else {
+    if (isset($_POST["page"])) {
+        $page = $_POST['page'];
+    } else {
+        $page = "home";
+    }
+}
+
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];	
+} else {
+    if (isset($_POST["action"])) {
+        $action = $_POST['action'];
+    } else {
+        $action = "home";
+    }
+}
+
+if($page == "logIn") {
+    include(__DIR__."/controllers/loginController.php");
+}
+else if($page == "signUp") {
+    include(__DIR__."/controllers/signupController.php");
+}
+else if($page == "advSearch") {
+    include(__DIR__."/controllers/advancedSearchController.php");
+}
+else if($page == "home") {
+    include(__DIR__."/controllers/homeController.php");
+}
+else if($page == "list") {
+    include(__DIR__."/controllers/listController.php");
+}
+else if($page == "reviews") {
+    include(__DIR__."/controllers/reviewController.php");
+} 
+?>
